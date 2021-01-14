b809f5d9554b0320b029dd722b02581e9a679acb
Ruslan Uteshev <dolphin_4@mail.ru>
2021-01-14 13:17:44 +0300

    Editing test_items file

--- test_items.py
+++ test_items.py
@@ -6,6 +6,6 @@ import time
 def test_existing_of_add_to_basket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(3)
+    time.sleep(30)
     button = browser.find_elements_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
     assert button, "The add to basket button is absent"
